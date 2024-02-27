d581b433feb7b5bb111af42742d1e9c28eac0663
Demiss Getachew <demisgech@gmail.com>
2024-02-27 16:41:37 +0300

    Check min max of two number.

--- minMaxOfTwoNmber.cpp
+++ minMaxOfTwoNmber.cpp
@@ -5,6 +5,24 @@ using namespace std;
 int main()
 {
 
-    cout << "Hello world" << endl;
+    // Min...Max of two number
+
+    int firstNumber = 12;
+    int secondNumber = 34;
+    if (firstNumber > secondNumber)
+    {
+        cout << "Maximum number = " << firstNumber << endl;
+        cout << "Minimum number = " << secondNumber << endl;
+    }
+    else if (secondNumber > firstNumber)
+    {
+        cout << "Maximum number = " << secondNumber << endl;
+        cout << "Minimum number = " << firstNumber << endl;
+    }
+    else
+    {
+        cout << "They are equal." << endl;
+    }
+
     return 0;
 }
