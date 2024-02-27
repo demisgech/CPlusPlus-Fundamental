05e2ea16a12976fa4f094a604826126c48382a2b
Demiss Getachew <demisgech@gmail.com>
2024-02-27 17:02:07 +0300

    Operate two number.

---
+++ operationOfTwoNumber.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    double number1 = 45;
+    double number2 = 30;
+
+    double sum = number1 + number2;      // sum = 75
+    double product = number1 * number2;  // product = 1350
+    double subtract = number1 - number2; // subtract = 15
+    double division = number1 / number2; // division = 1.5
+
+    return 0;
+}
